f92389e8084eec36b5d8f1ed2b9ffbc2be8a9c70
Dmytro <voronok16@gmail.com>
2019-09-23 22:39:18 +0300

    Updates

---
+++ precache-manifest.3bc23b892892950d4e9d387eb211eb3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5a9703b72037815eb6327bf4b1f387",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "df5218209dd691f28632",
+    "url": "/react_dynamic-list-of-posts/static/css/main.08dccc6c.chunk.css"
+  },
+  {
+    "revision": "26eec4809ce8af0dd949",
+    "url": "/react_dynamic-list-of-posts/static/js/2.9f912f88.chunk.js"
+  },
+  {
+    "revision": "df5218209dd691f28632",
+    "url": "/react_dynamic-list-of-posts/static/js/main.720b207b.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
